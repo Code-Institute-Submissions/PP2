226f4cf2e7b79340115574e092de0009309227c3
MadeleneEriksson <madelene__eriksson@hotmail.com>
2021-11-11 14:46:54 +0000

    questions for the questionnaire

---
+++ assets/js/game.js
@@ -0,0 +1,6 @@
+export default class Game {
+    constructor(){
+        this.turn = "x"; 
+        this.board = new Array(9).fill(null)
+    }
+}
---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+import Game from "./game,js";
+
+let game = new game(); 
+
+console.log(game.turn);
+console.log(game.board);
