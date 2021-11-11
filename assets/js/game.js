0b34f987168557f8c194553d535d22cb034b6b23
MadeleneEriksson <madelene__eriksson@hotmail.com>
2021-11-11 16:47:21 +0000

    adding style to the quiz

--- assets/js/game.js
+++
@@ -1,6 +0,0 @@
-export default class Game {
-    constructor(){
-        this.turn = "x"; 
-        this.board = new Array(9).fill(null)
-    }
-}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-import Game from "./game,js";
-
-let game = new game(); 
-
-console.log(game.turn);
-console.log(game.board);
+function check() {
+    var c = 0;
+    var q1 = document.quiz.q1.value;
+    if (q1 == "stockholm")(c++)
+    document.write(c);
+}
