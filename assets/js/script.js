9da087e86263aafcb9b1c21af6385a8f9f42a910
MadeleneEriksson <madelene__eriksson@hotmail.com>
2021-11-11 17:53:53 +0000

    changes in the layout for the quiz form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 function check() {
     var c = 0;
     var q1 = document.quiz.q1.value;
+    var q2 = document.quiz.q2.value;
+    var q3 = document.quiz.q3.value;
+    var q4 = document.quiz.q4.value;
+    var q5 = document.quiz.q5.value;
     if (q1 == "stockholm")(c++)
+    if (q2 == "10.5m")(c++)
+    if (q3 == "70p")(c++)
+    if (q4 == "coffee2")(c++)
+    if (q5 == "97p")(c++)
+
     document.write(c);
 }
