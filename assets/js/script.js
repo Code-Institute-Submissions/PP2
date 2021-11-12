e2c4339afe6a5ed8b1df13e4ec2cc46da7739e54
MadeleneEriksson <madelene__eriksson@hotmail.com>
2021-11-12 11:08:57 +0000

    Updated README file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+let myQuiz = document.getElementById('quiz');
+let myResults = document.getElementById('results');
+let myScore = document.getElementById("score");
+
 function check() {
     var c = 0;
     var q1 = document.quiz.q1.value;
@@ -5,13 +9,17 @@ function check() {
     var q3 = document.quiz.q3.value;
     var q4 = document.quiz.q4.value;
     var q5 = document.quiz.q5.value;
-    if (q1 == "stockholm")(c++)
-    if (q2 == "10.5m")(c++)
-    if (q3 == "70p")(c++)
-    if (q4 == "coffee2")(c++)
-    if (q5 == "97p")(c++)
+    if (q1 == "stockholm")(++c)
+    if (q2 == "10.5m")(++c)
+    if (q3 == "70p")(++c)
+    if (q4 == "coffee2")(++c)
+    if (q5 == "97p")(++c)
 
-    document.write(c);
+    // document.write(c);
+
+    myQuiz.style.display = "none";
+    // myResults.style.display = "block";
+    myScore.innerHTML = c;
 
 
 }
