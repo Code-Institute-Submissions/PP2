a024a294b4d10245b14bf85d2eb2d990ea4c1c26
MadeleneEriksson <madelene__eriksson@hotmail.com>
2021-11-13 21:18:06 +0000

    added functionality to my quiz game included vas for, let, if and else values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,43 @@
-let myQuiz = document.getElementById('quiz');
-let myResults = document.getElementById('results');
-let myScore = document.getElementById("score");
+console.log('testing')
+var cities = document.getElementByName("city");
+var score = 0;
+var scoreEl = document.getElementById ("score");
 
-function check() {
-    var c = 0;
-    var q1 = document.quiz.q1.value;
-    var q2 = document.quiz.q2.value;
-    var q3 = document.quiz.q3.value;
-    var q4 = document.quiz.q4.value;
-    var q5 = document.quiz.q5.value;
-    if (q1 == "stockholm")(++c);
-    if (q2 == "10.5m")(++c);
-    if (q3 == "70p")(++c);
-    if (q4 == "coffee2")(++c);
-    if (q5 == "97p")(++c);
+for(let city in cities) {
+    cities(city).onclick = function(e) {
+        let val = e.target.value; 
+        if ( val === 'Stockholm'){
+            alert("Right answer");
+            score = score +1; 
+            scoreEl.innerHTML = score; 
+        }
+        else {
+            alert ("Sorry, wrong answer");
+            if (score > 0){
+                score = score - 1; 
+                scoreEl.innerHTML = score; 
+            }
+        }
+    }
+}
+
+
+//let myQuiz = document.getElementById('quiz');
+//let myResults = document.getElementById('results');
+//let myScore = document.getElementById("score");
+
+//function check() {
+  //  var c = 0;
+   // var q1 = document.quiz.q1.value;
+    //var q2 = document.quiz.q2.value;
+    //var q3 = document.quiz.q3.value;
+    //var q4 = document.quiz.q4.value;
+    //var q5 = document.quiz.q5.value;
+    //if (q1 == "stockholm")(++c);
+    //if (q2 == "10.5m")(++c);
+    //if (q3 == "70p")(++c);
+    //if (q4 == "coffee2")(++c);
+    //if (q5 == "97p")(++c);
 
     // document.write(c);
 
